6a3b500de23201ceb296ab14fc4aa9b6df79e3d7
atleta41uaf <atleta41uaf@gmail.com>
2023-04-09 00:57:49 -0500

    cambios

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyD3Z7zpf2IWtKh5gXEfb99XYbm8Hjy6ifU",
+    authDomain: "atleta41uaf1.firebaseapp.com",
+    projectId: "atleta41uaf1",
+    storageBucket: "atleta41uaf1.appspot.com",
+    messagingSenderId: "450309402252",
+    appId: "1:450309402252:web:5135106691ed1a822d95a9",
+    measurementId: "G-V4K80QMVTQ"
+});
